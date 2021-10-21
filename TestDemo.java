39d2137d64ec4272137ff5bb66ed2eb2793485a4
Christen Oorlog <88000747+COorlog@users.noreply.github.com>
2021-10-20 18:02:49 -0600

    Add files via upload

---
+++ TestDemo.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class TestDemo {
+	
+	public int addPositive(int a, int b) {
+				
+		if((a >= 1) && (b >= 1)) {
+			return a + b;
+			
+		}else 
+			throw new IllegalArgumentException("Both parameters must be positive!");
+	}
+		
+	
+	public int randomNumberSquared() {
+		int number = getRandomInt();
+		return number * number;
+	}
+	
+	public int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	
+		}
+		}
+	
+	
+
+	
+		
+	
+
+
